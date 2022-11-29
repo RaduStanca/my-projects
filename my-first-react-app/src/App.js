fa423144230ddbbaa3f76b674bfb811fe619e947
Radu Stanca <Radu_Stanca_96@yahoo.com>
2022-11-29 10:38:03 +0200

    base/1.3-introducing-props

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -2,9 +2,51 @@ import "./App.css";
 import TaskCard from "./components/task-card/TaskCard";
 
 function App() {
+  const data = [
+    {
+      id: "T-1",
+      status: "Todo",
+      name: "Create a Design System for Enum Workspace.",
+      dueDate: new Date(2022, 10, 24),
+    },
+
+    {
+      id: "T-2",
+      status: "In progres ",
+      name: "12Create a Design System for Enum Workspace.",
+      dueDate: new Date(2022, 10, 30),
+    },
+
+    {
+      id: "T-3",
+      status: "Completed",
+      name: "13Create a Design System for Enum Workspace.",
+      dueDate: new Date(2022, 11, 10),
+    },
+  ];
+
   return (
     <div className="app-container">
-      <TaskCard />
+      <TaskCard
+        id={data[0].id}
+        status={data[0].status}
+        name={data[0].name}
+        dueDate={data[0].dueDate}
+      />
+
+      <TaskCard
+        id={data[1].id}
+        status={data[1].status}
+        name={data[1].name}
+        dueDate={data[1].dueDate}
+      />
+
+      <TaskCard
+        id={data[2].id}
+        status={data[2].status}
+        name={data[2].name}
+        dueDate={data[2].dueDate}
+      />
     </div>
   );
 }
