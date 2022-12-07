8e68495d158f2a1804e246dd3cb5cfec05559885
Radu Stanca <Radu_Stanca_96@yahoo.com>
2022-12-07 15:02:06 +0200

    base/1.4-spliting-components

---
+++ my-first-react-app/src/components/task-viewer/TaskViewer.js
@@ -0,0 +1,40 @@
+import "./TaskViewer.css";
+import TaskCard from "../task-card/TaskCard";
+
+function TaskViewer(props) {
+  return (
+    <div className="task-viewer-container">
+      <div className="task-list-container">
+        <TaskCard
+          id={props.taskList[0].id}
+          status={props.taskList[0].status}
+          name={props.taskList[0].name}
+          dueDate={props.taskList[0].dueDate}
+        />
+
+        <TaskCard
+          id={props.taskList[1].id}
+          status={props.taskList[1].status}
+          name={props.taskList[1].name}
+          dueDate={props.taskList[1].dueDate}
+        />
+
+        <TaskCard
+          id={props.taskList[2].id}
+          status={props.taskList[2].status}
+          name={props.taskList[2].name}
+          dueDate={props.taskList[2].dueDate}
+        />
+
+        <TaskCard
+          id={props.taskList[3].id}
+          status={props.taskList[3].status}
+          name={props.taskList[3].name}
+          dueDate={props.taskList[3].dueDate}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default TaskViewer;
