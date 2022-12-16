96a6fa5584fa2f8fd9872cf3ad048b6bf2a067e9
Radu Stanca <Radu_Stanca_96@yahoo.com>
2022-12-16 10:44:03 +0200

    base/2.2-events-in-React

--- my-first-react-app/src/components/task-card/TaskCard.js
+++ my-first-react-app/src/components/task-card/TaskCard.js
@@ -3,6 +3,11 @@ import Badge from "../badge/Badge";
 import DateContainer from "../date-container/DateContainer";
 
 function TaskCard(props) {
+  let taskId = props.id;
+  const handleClick = () => {
+    console.log("Clicked!! " + props.id);
+    taskId = taskId + "Clicked!!";
+  };
   return (
     <div className="card-wrapper">
       <div className="card-header">
@@ -13,6 +18,7 @@ function TaskCard(props) {
         <p>{props.name}</p>
       </div>
       <div className="card-footer">
+        <button onClick={handleClick}>Click Me!</button>
         <DateContainer dueDate={props.dueDate} />
       </div>
     </div>
