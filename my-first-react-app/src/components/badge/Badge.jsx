c098089bf6ae90e4e6b69e532c8a149d9284d884
Radu Stanca <Radu_Stanca_96@yahoo.com>
2023-01-20 15:20:51 +0200

    base/3.2-conditional-rendering

--- my-first-react-app/src/components/badge/Badge.jsx
+++ my-first-react-app/src/components/badge/Badge.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Badge.css";
 
-function Badge(props) {
+const BADGE_COLORS = ["grey", "blue", "black", "green", "orange"];
+
+const Badge = (props) => {
+  const badgeColor = BADGE_COLORS.includes(props.color) ? props.color : "grey";
+
   return (
-    <div className="badge">
-      <p>{props.status}</p>
+    <div className={`badge ${badgeColor}`}>
+      <p>{props.label}</p>
     </div>
   );
-}
+};
 
 export default Badge;
